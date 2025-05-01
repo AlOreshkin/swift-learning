add211daa257e98a0afe19ad0677b97c9ebc3150
Александр Орешкин <aoresh@Mac-mini-Aleksandr.local>
2025-05-01 14:35:07 +0300

    Commit1

---
+++  1.1_oreshkin/lesson1.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// 2. Объявление переменных профиля пользователя
+let firstName = "Alex"
+let secondName = "Oreshkin"
+var age = 29
+let gender = "man"
+var aboutMe = "I'm learning iOS development"
+let isPublic = true
+
+// 3. Вывод характеристик профиля в консоль
+print(firstName, secondName, age, gender, aboutMe, isPublic)
+
+// 4. Вывод характеристик профиля в консоль (дружелюбно)
+print("Фамилия: \(secondName)")
+print("Имя: \(firstName)")
+print("Возраст: \(age)")
+print("Пол: \(gender)")
+print("О себе: \(aboutMe)")
+print("Публичный профиль: \(isPublic)")
+
